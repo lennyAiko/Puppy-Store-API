93bdfd433c81a2378aa4bcd244be22687c03301d
Lennox Charles <lennyaiko17@gmail.com>
2023-02-21 17:56:49 +0100

    bug fixes

--- puppy_store/puppy_store/urls.py
+++ puppy_store/puppy_store/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include, url
+from django.urls import path, re_path, include
 
 urlpatterns = [
-    url(r'^', include('puppies.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls)
+    re_path(r'^', include('puppies.urls')),
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^admin/', admin.site.urls)
 ]
